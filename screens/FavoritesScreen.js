aa3395c14d45091fa0daa2a7698a1e1485f23e93
kristenking <kristenking0110@gmail.com>
2022-09-09 09:32:31 -0500

    Added Swipe Delete to FavoritesScreen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,17 +1,31 @@
-import { useSelector } from "react-redux";
-import { View, FlatList, Text } from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+import { View, FlatList, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
 import Loading from "../components/LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { SwipeRow } from "react-native-swipe-list-view";
+import { toggleFavorite } from "../features/favorites/favoritesSlice";
+
 
 const FavoritesScreen = ({ navigation }) => {
     const { campsitesArray, isLoading, errMess } = useSelector((state) => state.campsites
     );
     const favorites = useSelector((state) => state.favorites);
+    const dispatch = useDispatch();
 
     const renderFavoriteItem = ({ item: campsite }) => {
         return (
-            <ListItem
+            <SwipeRow rightOpenValue={-100}>
+                <View style={styles.deleteView}>
+                    <TouchableOpacity
+                    style={styles.deleteTouchable}
+                    onPress={() => dispatch(toggleFavorite(campsite.id))}
+                    >
+                       <Text style={styles.deleteText}>Delete</Text> 
+                    </TouchableOpacity>
+                </View>
+                <View>
+                <ListItem
                 onPress={() => navigation.navigate('Directory', {
                     screen: 'CampsiteInfo',
                     params: { campsite }
@@ -22,6 +36,9 @@ const FavoritesScreen = ({ navigation }) => {
                     <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
                 </ListItem.Content>
             </ListItem>
+                </View>
+           
+            </SwipeRow>
         )
     }
 
@@ -43,5 +60,26 @@ const FavoritesScreen = ({ navigation }) => {
         />
     )
 };
+
+const styles= StyleSheet.create({
+    deleteView: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        flex: 1
+    },
+    deleteTouchable: {
+        backgroundColor: 'red',
+        height: "100%",
+        justifyContent: 'center'
+    },
+    deleteText: {
+        color: 'white',
+        fontWeight: '700',
+        textAlign: 'center',
+        fontSize: 16,
+        width: 100
+    }
+})
 
 export default FavoritesScreen;
