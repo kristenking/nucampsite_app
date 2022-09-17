2e253882d1f4ca3a1320dde5dea468f36c68b0f9
kristenking <kristenking0110@gmail.com>
2022-09-17 07:36:28 -0500

    Added Notofications to ReservartionScreen

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -11,16 +11,13 @@ import {
 import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
-
+import * as Notifications from 'expo-notifications';
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
     const [hikeIn, setHikeIn] = useState(false);
     const [date, setDate] = useState(new Date());
     const [showCalendar, setShowCalendar] = useState(false);
-    // const [showModal, setShowModal] = useState(false);
-
-
 
     const onDateChange = (event, selectedDate) => {
         const currentDate = selectedDate || date;
@@ -29,26 +26,36 @@ const ReservationScreen = () => {
     };
 
     const handleReservation = () => {
-        console.log('campers:', campers);
-        console.log('hikeIn:', hikeIn);
-        console.log('date:', date);
+        const message = `Number of Campers: ${campers}
+                            \nHike-In? ${hikeIn}
+                            \nDate: ${date.toLocaleDateString('en-US')}`;
         Alert.alert(
             'Begin Search?',
-            `Number of Campers: ${campers}\n \nHike-in?  ${hikeIn}\n \nDate: ${date.toLocaleDateString('en-US')}`,
+            message,
             [
                 {
                     text: 'Cancel',
-                    style: 'cancel',
-                    onPress: () => resetForm()
+                    onPress: () => {
+                        console.log('Reservation Search Canceled');
+                        resetForm();
+                    },
+                    style: 'cancel'
                 },
                 {
                     text: 'OK',
-                    onPress: () => resetForm()
+                    onPress: () => {
+                        presentLocalNotification(
+                            date.toLocaleDateString('en-US')
+                        );
+                        resetForm();
+                    }
                 }
             ],
             { cancelable: false }
-        )
-
+        );
+        console.log('campers:', campers);
+        console.log('hikeIn:', hikeIn);
+        console.log('date:', date);
     };
 
     const resetForm = () => {
@@ -58,13 +65,37 @@ const ReservationScreen = () => {
         setShowCalendar(false);
     };
 
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${reservationDate} requested`
+                },
+                trigger: null
+            });
+        };
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    };
+
     return (
         <ScrollView>
-            <Animatable.View
-                animation='zoomIn'
-                duration={2000}
-                delay={1000}
-            >
+            <Animatable.View animation='zoomIn' duration={2000} delay={1000}>
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Number of Campers:</Text>
                     <Picker
@@ -109,42 +140,12 @@ const ReservationScreen = () => {
                 )}
                 <View style={styles.formRow}>
                     <Button
-                        onPress={() => handleReservation()
-                        }
+                        onPress={() => handleReservation()}
                         title='Search Availability'
                         color='#5637DD'
                         accessibilityLabel='Tap me to search for available campsites to reserve'
                     />
                 </View>
-                {/* <Modal
-                animationType='slide'
-                transparent={false}
-                visible={showModal}
-                onRequestClose={() => setShowModal(!showModal)}
-            > */}
-                {/* <View style={styles.modal}>
-                    <Text style={styles.modalTitle}>
-                        Search Campsite Reservations
-                    </Text>
-                    <Text style={styles.modalText}>
-                        Number of Campers: {campers}
-                    </Text>
-                    <Text style={styles.modalText}>
-                        Hike-In?: {hikeIn ? 'Yes' : 'No'}
-                    </Text>
-                    <Text style={styles.modalText}>
-                        Date: {date.toLocaleDateString('en-US')}
-                    </Text>
-                    <Button
-                        onPress={() => {
-                            
-                            resetForm();
-                        }}
-                        color='#5637DD'
-                        title='Close'
-                    />
-                </View> */}
-                {/* </Modal> */}
             </Animatable.View>
         </ScrollView>
     );
@@ -164,22 +165,6 @@ const styles = StyleSheet.create({
     },
     formItem: {
         flex: 1
-    },
-    modal: {
-        justifyContent: 'center',
-        margin: 20
-    },
-    modalTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        backgroundColor: '#5637DD',
-        textAlign: 'center',
-        color: '#fff',
-        marginBottom: 20
-    },
-    modalText: {
-        fontSize: 18,
-        margin: 10
     }
 });
 
